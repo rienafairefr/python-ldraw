beafd7c721a164a8e48afe43d366f986e920d1ac
David Boddie <david@boddie.org.uk>
2009-03-14 02:41:26 +0100

    Added preliminary support for reading parts files. Since these are defined in the same way as normal LDraw files, we can also use this feature to test reading and writing by trying to "round trip" existing LDraw files, including those created by this package.
    This feature will also be useful for converting LDraw files into
    other formats.

--- ldraw/parts.py
+++ ldraw/parts.py
@@ -19,10 +19,24 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import os
+from colours import Colour
+from geometry import Matrix, Vector
+from pieces import Piece
+
+class PartError(Exception):
+
+    pass
+
+
 class Parts:
 
     def __init__(self, path):
     
+        self.path = None
+        self.parts_dir = None
+        self.parts = {}
+        
         self.Hats = {}
         self.Heads = {}
         self.Torsos = {}
@@ -69,6 +83,7 @@ class Parts:
                 
                 code = pieces[0]
                 description = pieces[1].strip()
+                self.parts[description] = code
                 
                 for key, section in self.sections.items():
                 
@@ -85,4 +100,215 @@ class Parts:
                         section[description] = code
                         break
         except IOError:
-            pass
+            return
+        
+        # If we successfully loaded the file then record the path and look for
+        # part files.
+        
+        self.path = path
+        directory = os.path.split(self.path)[0]
+        for item in os.listdir(directory):
+            if item.lower() == "parts":
+                self.parts_dir = os.path.join(directory, item)
+                break
+    
+    def part(self, description = None, code = None):
+    
+        if not self.path:
+            return None
+        
+        if description:
+            try:
+                code = self.parts[description]
+            except KeyError:
+                return None
+        
+        elif not code:
+            return None
+        
+        return self._load_part(code)
+    
+    def _load_part(self, code):
+    
+        path_stem = os.path.join(self.parts_dir, code)
+        paths = (path_stem + os.extsep + "dat",
+                 path_stem + os.extsep + "DAT")
+        
+        for path in paths:
+            part = Part(path)
+            if part:
+                return part
+        
+        return None
+
+
+class Part:
+
+    def __init__(self, path):
+    
+        self._handlers = {
+            "0": self._comment_or_meta,
+            "1": self._subfile,
+            "2": self._line,
+            "3": self._triangle,
+            "4": self._quadrilateral,
+            "5": self._optional_line
+            }
+        self.load(path)
+    
+    def load(self, path):
+    
+        try:
+            lines = open(path).readlines()
+        except IOError:
+            return
+        
+        objects = []
+        number = 1
+        for line in lines:
+        
+            pieces = line.split()
+            if not pieces:
+                objects.append(BlankLine)
+                continue
+            
+            try:
+                handler = self._handlers[pieces[0]]
+            except KeyError:
+                raise PartError, "Unknown command (%s) at line %i" % (pieces[0], number)
+            
+            objects.append(handler(pieces[1:], number))
+            number += 1
+        
+        self.path = path
+        self.objects = objects
+    
+    def _comment_or_meta(self, pieces, line):
+    
+        if not pieces:
+            return Comment("")
+        elif pieces[0][:1] == "!":
+            return MetaCommand(" ".join(pieces))
+        else:
+            return Comment(" ".join(pieces))
+    
+    def _subfile(self, pieces, line):
+    
+        if len(pieces) != 14:
+            raise PartError, "Invalid part data at line %i" % line
+        
+        colour = int(pieces[0])
+        position = map(float, pieces[1:4])
+        rows = [map(float, pieces[4:7]),
+                map(float, pieces[7:10]),
+                map(float, pieces[10:13])]
+        part = pieces[13].upper()
+        if part.endswith(".DAT"):
+            part = part[:-4]
+        
+        return Piece(Colour(colour), Vector(*position), Matrix(rows), part)
+    
+    def _line(self, pieces, line):
+    
+        if len(pieces) != 7:
+            raise PartError, "Invalid line data at line %i" % line
+        
+        colour = int(pieces[0])
+        p1 = map(float, pieces[1:4])
+        p2 = map(float, pieces[4:7])
+        
+        return Line(Colour(colour), Vector(*p1), Vector(*p2))
+    
+    def _triangle(self, pieces, line):
+    
+        if len(pieces) != 10:
+            raise PartError, "Invalid triangle data at line %i" % line
+        
+        colour = int(pieces[0])
+        p1 = map(float, pieces[1:4])
+        p2 = map(float, pieces[4:7])
+        p3 = map(float, pieces[7:10])
+        
+        return Triangle(Colour(colour), Vector(*p1), Vector(*p2), Vector(*p3))
+    
+    def _quadrilateral(self, pieces, line):
+    
+        if len(pieces) != 13:
+            raise PartError, "Invalid quadrilateral data at line %i" % line
+        
+        colour = int(pieces[0])
+        p1 = map(float, pieces[1:4])
+        p2 = map(float, pieces[4:7])
+        p3 = map(float, pieces[7:10])
+        p4 = map(float, pieces[10:13])
+        
+        return Quadrilateral(Colour(colour), Vector(*p1), Vector(*p2),
+                                             Vector(*p3), Vector(*p4))
+    
+    def _optional_line(self, pieces, line):
+    
+        if len(pieces) != 7:
+            raise PartError, "Invalid line data at line %i" % line
+        
+        colour = int(pieces[0])
+        p1 = map(float, pieces[1:4])
+        p2 = map(float, pieces[4:7])
+        p3 = map(float, pieces[7:10])
+        p4 = map(float, pieces[10:13])
+        
+        return OptionalLine(Colour(colour), Vector(*p1), Vector(*p2),
+                                            Vector(*p3), Vector(*p4))
+
+
+class BlankLine:
+
+    pass
+
+class Comment:
+
+    def __init__(self, text):
+    
+        self.text = text
+
+class MetaCommand:
+
+    def __init__(self, text):
+    
+        self.text = text
+
+class Line:
+
+    def __init__(self, colour, p1, p2):
+    
+        self.colour = colour
+        self.p1 = p1
+        self.p2 = p2
+
+class Triangle:
+
+    def __init__(self, colour, p1, p2, p3):
+    
+        self.colour = colour
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+class Quadrilateral:
+
+    def __init__(self, colour, p1, p2, p3, p4):
+    
+        self.colour = colour
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+
+class OptionalLine:
+
+    def __init__(self, colour, p1, p2, p3, p4):
+    
+        self.colour = colour
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
