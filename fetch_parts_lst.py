f6071b7ae962d6074e2aaae2b8b4278a2f7be863
rienafairefr <rienafairefr@gmail.com>
2018-02-25 23:01:35 +0100

    fix fetch_parts_lst

--- fetch_parts_lst.py
+++ fetch_parts_lst.py
@@ -2,6 +2,9 @@
 from urllib import urlretrieve
 import zipfile
 import os
+import io
+
+from subprocess import check_call, CalledProcessError
 
 
 url = 'http://www.ldraw.org/library/updates/complete.zip'
@@ -25,7 +28,7 @@ if not os.path.exists('tmp/mklist/mklist'):
     zip_ref.close()
 
     print('patch the mklist  sources ...')
-    patch = """diff -Naur mklist.orig/makefile mklist/makefile
+    patch = u"""diff -Naur mklist.orig/makefile mklist/makefile
 --- mklist.orig/makefile	2018-02-23 10:05:40.000000000 +0100
 +++ mklist/makefile	2018-02-23 10:05:47.111079214 +0100
 @@ -1,6 +1,6 @@
@@ -48,13 +51,14 @@ diff -Naur mklist.orig/mklist.c mklist/mklist.c
  }
  #endif
 """
-    open('tmp/mklist.patch', 'w').write(patch)
+    with io.open('tmp/mklist.patch', 'w', newline='\r\n') as patch_file:
+        patch_file.write(patch)
 
-    os.system('dos2unix tmp/mklist/mklist.c')
-    os.system('dos2unix tmp/mklist/makefile')
-    os.system('patch -d tmp -l -p0 < tmp/mklist.patch')
-    os.system('make -C tmp/mklist')
+    check_call('dos2unix tmp/mklist/mklist.c', shell=True)
+    check_call('dos2unix tmp/mklist/makefile', shell=True)
+    check_call('patch -d tmp -l -p0 < tmp/mklist.patch', shell=True)
+    check_call('make -C tmp/mklist', shell=True)
 
 if not os.path.exists('tmp/ldraw/parts.lst'):
     print('mklist ...')
-    os.system('tmp/mklist/mklist -d -i tmp/ldraw/parts -o tmp/ldraw/parts.lst')
+    check_call('tmp/mklist/mklist -d -i tmp/ldraw/parts -o tmp/ldraw/parts.lst', shell=True)
