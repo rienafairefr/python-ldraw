8366f95dcaa5a16c8e9efd784fdb3935ff4b01d7
rienafairefr <rienafairefr@gmail.com>
2018-03-09 11:02:51 +0100

    pylint svg

--- ldraw/writers/svg.py
+++ ldraw/writers/svg.py
@@ -20,13 +20,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import sys
 
-from ldraw.geometry import Identity, Vector
+from ldraw.geometry import Identity, Vector, Vector2D
 from ldraw.parts import Parts
 from ldraw.lines import Quadrilateral, Line, Triangle
 from ldraw.pieces import Piece
 
 
-class Polygon:
+class Polygon(object):
     def __init__(self, zmin, points, colour):
         self.zmin = zmin
         self.points = points
@@ -36,7 +36,40 @@ class Polygon:
         return self.zmin < other.zmin
 
 
-class SVGWriter:
+def _current_colour(colour, current_colour):
+    if colour.code == 16:
+        return current_colour
+    else:
+        return colour.code
+
+
+SVG_PREAMBLE = """<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="{width:.6f}cm" \
+height="{width:.6f}cm" \
+viewBox="{view1:.6f} {view2:.6f} {width:.6f} {height:.6f}" \
+xmlns="http://www.w3.org/2000/svg" version="1.1">
+"""
+
+
+SVG_LINE = """<line stroke="{rgb}" \
+stroke-width="{stroke_width}" \
+opacity="{opacity:.6f}" \
+x1="{point1.x:.6f}" \
+y1="{point1.y:.6f}" \
+x2="{point2.x:.6f}" \
+y2="{point2.y:.6f}" />
+"""
+
+SVG_POLYGON_PREAMBLE ="""<polygon fill="{rgb}" \
+stroke="{stroke_colour}" \
+stroke-width="{stroke_width}" \
+opacity="{opacity:.6f}" \
+points=\""""
+
+
+class SVGWriter(object):
     def __init__(self, camera_position, axes, parts):
         self.parts = parts
         self.lights = []
@@ -50,18 +83,10 @@ class SVGWriter:
         polygons = self._polygons_from_objects(model)
         self._sort_polygons(polygons)
         shapes = self._project_polygons(width, height, polygons)
-        # shapes = self._combine_polygons(shapes)
-        # shapes = self._remove_obscured_polygons(shapes)
         self._write(shapes, svg_file, width, height, stroke_colour, stroke_width, background_colour)
 
     def _write(self, shapes, svg_file, width, height, stroke_colour=None, stroke_width=None, background_colour=None):
-        svg_file.write('<?xml version="1.0" standalone="no"?>\n'
-                       '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"\n'
-                       '  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n')
-        svg_file.write('<svg width="%.6fcm" height="%.6fcm" ' % (width, height))
-        svg_file.write('viewBox="%.6f %.6f %.6f %.6f" '
-                       'xmlns="http://www.w3.org/2000/svg" '
-                       'version="1.1">\n' % (0.0, 0.0, width, height))
+        svg_file.write(SVG_PREAMBLE.format(view1=0.0, view2=0.0, width=width, height=height))
         if stroke_width is None:
             stroke_width = "0.1%"
         if background_colour is not None:
@@ -71,32 +96,22 @@ class SVGWriter:
         for points, colour in shapes:
             rgb = self.parts.colours.get(colour, "#ffffff")
             if len(points) == 2:
-                svg_file.write('<line stroke="%s" ' % rgb)
-                svg_file.write('stroke-width="%s" ' % stroke_width)
-                svg_file.write('opacity="%.6f" ' % self._opacity_from_colour(colour))
-                svg_file.write('x1="%.6f" y1="%.6f" ' % (points[0][0] + width / 2.0, height / 2.0 - points[0][1]))
-                svg_file.write('x2="%.6f" y2="%.6f" ' % (points[1][0] + width / 2.0, height / 2.0 - points[1][1]))
-                svg_file.write('/>\n')
+                point1 = Vector2D(points[0][0] + width / 2.0, height / 2.0 - points[0][1])
+                point2 = Vector2D(points[1][0] + width / 2.0, height / 2.0 - points[1][1])
+
+                svg_file.write(SVG_LINE.format(rgb=rgb, stroke_width=stroke_width, opacity=self._opacity_from_colour(colour),
+                                               point1=point1, point2=point2))
             else:
-                svg_file.write('<polygon fill="%s" ' % rgb)
-                if stroke_colour:
-                    svg_file.write('stroke="%s" ' % stroke_colour)
-                else:
-                    svg_file.write('stroke="%s" ' % rgb)
-                svg_file.write('stroke-width="%s" ' % stroke_width)
-                svg_file.write('opacity="%.6f" ' % self._opacity_from_colour(colour))
-                svg_file.write('points="')
+                svg_file.write(SVG_POLYGON_PREAMBLE.format(rgb=rgb,
+                                                           stroke_colour=stroke_colour if stroke_colour else rgb,
+                                                           stroke_width=stroke_width,
+                                                           opacity=self._opacity_from_colour(colour)))
                 for x, y in points:
-                    svg_file.write('%.6f,%.6f ' % (x + width / 2.0, height / 2.0 - y))
+                    point = Vector2D(x + width / 2.0, height / 2.0 - y)
+                    svg_file.write('{point.x:.6f},{point.y:.6f} '.format(point=point) )
                 svg_file.write('" />\n')
         svg_file.write("</svg>\n")
         svg_file.close()
-
-    def _current_colour(self, colour, current_colour):
-        if colour.code == 16:
-            return current_colour
-        else:
-            return colour.code
 
     def _opacity_from_colour(self, colour):
         return self.parts.alpha_values.get(colour, 255) / 255.0
@@ -111,7 +126,7 @@ class SVGWriter:
             if isinstance(obj, Piece):
                 if obj.part == "LIGHT":
                     continue
-                colour = self._current_colour(obj.colour, current_colour)
+                colour = _current_colour(obj.colour, current_colour)
                 part = self.parts.part(code=obj.part)
                 if part:
                     matrix = obj.matrix
@@ -127,7 +142,7 @@ class SVGWriter:
                 r2 = Vector(p2.dot(x), p2.dot(y), p2.dot(z))
                 if r1.z >= 0 or r2.z >= 0:
                     continue
-                colour = self._current_colour(obj.colour, current_colour)
+                colour = _current_colour(obj.colour, current_colour)
                 polygons.append(Polygon(min(r1.z, r2.z), [r1, r2], colour))
             elif isinstance(obj, Triangle):
                 p1 = current_matrix * obj.p1 + current_position - c
@@ -140,7 +155,7 @@ class SVGWriter:
                 r3 = Vector(p3.dot(x), p3.dot(y), p3.dot(z))
                 if r1.z >= 0 or r2.z >= 0 or r3.z >= 0:
                     continue
-                colour = self._current_colour(obj.colour, current_colour)
+                colour = _current_colour(obj.colour, current_colour)
                 polygons.append(Polygon(min(r1.z, r2.z, r3.z), [r1, r2, r3], colour))
             elif isinstance(obj, Quadrilateral):
                 p1 = current_matrix * obj.p1 + current_position - c
@@ -157,7 +172,7 @@ class SVGWriter:
                 r4 = Vector(p4.dot(x), p4.dot(y), p4.dot(z))
                 if r1.z >= 0 or r2.z >= 0 or r3.z >= 0 or r4.z >= 0:
                     continue
-                colour = self._current_colour(obj.colour, current_colour)
+                colour = _current_colour(obj.colour, current_colour)
                 polygons.append(Polygon(min(r1.z, r2.z, r3.z, r4.z), [r1, r2, r3, r4], colour))
         return polygons
 
@@ -179,81 +194,3 @@ class SVGWriter:
                                    h * (point.y / (h + b * -point.z))))
             new_polygons.append((new_points, polygon.colour))
         return new_polygons
-
-    def _combine_polygons(self, polygons):
-        # Create a dictionary mapping adjacent vertices to polygons with those
-        # vertices.
-        mapping = {}
-        new_polygons = []
-        for points, colour in polygons:
-            len_points = len(points)
-            if len_points == 2:
-                new_polygons.append((points, colour))
-                continue
-            for i in range(len_points):
-                p1, p2 = points[i], points[(i + 1) % len_points]
-                polygon = Poly2(points)
-                polygon.colour = colour
-                mapping.setdefault((p1, p2), []).append(polygon)
-        discarded = {}
-        for key, shared in mapping.items():
-            colour = shared[0].colour
-            shared = filter(lambda polygon: polygon not in discarded and polygon.colour == colour, shared)
-            if len(shared) == 1:
-                new_polygons.append((shared[0].points, colour))
-                discarded[shared[0]] = None
-            elif len(shared) >= 2:
-                # Join the two polygons where they share vertices.
-                while len(shared) > 1:
-                    poly1 = shared.pop()
-                    poly2 = shared.pop()
-                    polygon = poly1.join(key, poly2)
-                    new_polygons.append((polygon.points, colour))
-                    discarded[poly1] = None
-                    discarded[poly2] = None
-                if shared:
-                    new_polygons.append((shared[0].points, colour))
-                    discarded[shared[0]] = None
-        return new_polygons
-
-    def _remove_obscured_polygons(self, polygons):
-        # Perform an expensive test to remove obscured polygons.
-        polygons.reverse()
-        i = 0
-        while i < len(polygons):
-            points, colour = polygons[i]
-            j = i + 1
-            while j < len(polygons):
-                other_points, other_colour = polygons[j]
-                if other_colour != colour:
-                    j += 1
-                    continue
-                for p in other_points:
-                    if not self._point_within_polygon(p, points):
-                        j += 1
-                        break
-                else:
-                    print "Discarding polygon", polygons[j]
-                    del polygons[j]
-            i += 1
-        polygons.reverse()
-        return polygons
-
-    def _point_within_polygon(self, p, points):
-        x, y = p
-        x1, y1 = points[-1]
-        inside = False
-        for x2, y2 in points:
-            if y1 == y2:
-                continue
-            if y1 <= y <= y2 or y2 <= y <= y1:
-                if x <= x1 == x2:
-                    inside = not inside
-                elif x <= min(x1, x2):
-                    inside = not inside
-                elif x <= max(x1, x2):
-                    x_inter = x1 + (y - y1) * (x2 - x1) / (y2 - y1)
-                    if x <= x_inter:
-                        inside = not inside
-            x1, y1 = x2, y2
-        return inside
