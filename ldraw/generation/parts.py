d766cb47e84507a20eab19413036f43939e6de42
rienafairefr <rienafairefr@gmail.com>
2018-03-13 15:12:16 +0100

    pylint ldraw.generation.parts

--- ldraw/generation/parts.py
+++ ldraw/generation/parts.py
@@ -1,114 +1,125 @@
 #!/usr/bin/env python
+"""
+Generates the ldraw.library.parts namespace
+"""
+from __future__ import print_function
 import codecs
 import os
 import itertools
 
 from progress.bar import Bar
 import pystache
-import inflect
 
 from ldraw.utils import clean, camel, ensure_exists, flatten
+from ldraw.parts import PartError
 
-p = inflect.engine()
+PARTS__INIT__TEMPLATE = os.path.join('templates', 'parts__init__.mustache')
+PARTS__INIT__TEMPLATE = codecs.open(PARTS__INIT__TEMPLATE, 'r', encoding='utf-8')
+PARTS__INIT__TEMPLATE = pystache.parse(PARTS__INIT__TEMPLATE.read())
+
+PARTS_TEMPLATE = os.path.join('templates', 'parts.mustache')
+PARTS_TEMPLATE = codecs.open(PARTS_TEMPLATE, 'r', encoding='utf-8')
+PARTS_TEMPLATE = pystache.parse(PARTS_TEMPLATE.read())
 
 
-def gen_parts(parts, output_dir, force=False):
+def write_section_file(parts_dir, list_of_parts, mod_name):
+    """ Writes a single section files"""
+    list_of_parts.sort(key=lambda o: o['description'])
+    part_str = pystache.render(PARTS_TEMPLATE, context={'parts': list_of_parts})
+    parts_py = os.path.join(parts_dir, mod_name + '.py')
+    ensure_exists(os.path.dirname(parts_py))
+    with codecs.open(parts_py, 'w', encoding='utf-8') as generated_file:
+        generated_file.write(part_str)
+
+
+def get_part_dict(parts, description):
+    """ Gets a dict context for a part """
+
+    try:
+        code = parts.parts_by_name[description]
+        part = parts.part(code=code)
+        return {
+            'path': part.path,
+            'description': description,
+            'class_name': clean(camel(description)),
+            'code': code
+        }
+    except (PartError, KeyError):
+        return {}
+
+
+def gen_parts(parts, output_dir):
     """
-    try:
-        from ldraw.library.colours import ColoursByCode
-        if not force:
-            return
-    except ImportError, e:
-        gen_colours(parts, config, data_dir)
-        from ldraw import library
-        imp.reload(library)
-        from ldraw.library.colours import ColoursByCode
-
-    try:
-        from ldraw.library.parts import Part
-        if not force:
-            return
-    except ImportError:
-        pass
-"""
+    Generates the ldraw.library.parts namespace
+    :param parts: Parts object
+    :param output_dir: where to output the library
+    :return:
+    """
     print('generate ldraw.library.parts, this might take a long time...')
-
-    parts_template = codecs.open(os.path.join('templates', 'parts.mustache'), 'r', encoding='utf-8').read()
-    parts_template = pystache.parse(parts_template)
-
-    def get_part_dict(description):
-        try:
-            code = parts.parts_by_name[description]
-            part = parts.part(code=code)
-            return {
-                'path': part.path,
-                'description': description,
-                'class_name': clean(camel(description)),
-                'code': code
-            }
-        except Exception, e:
-            return {}
-
-    sections = {}
-    for k, v in flatten(parts.parts, sep='|').items():
-        rsplitted = k.rsplit('|', 1)
-        description = rsplitted[-1]
-        sections.setdefault(rsplitted[0], {})[description] = v
 
     library_path = ensure_exists(os.path.join(output_dir, 'library'))
     parts_dir = ensure_exists(os.path.join(library_path, 'parts'))
 
+    sections = _get_sections(parts)
+
+    packages = _get_packages(sections)
+
+    for package_name, modules in packages.items():
+        generate_parts__init__(library_path, modules, package_name)
+
+    for section_name, section_parts in sections.items():
+        generate_section(parts, parts_dir, section_name, section_parts)
+
+
+def _get_packages(sections):
     packages = {}
     for section_name in sections:
-        rsplitted = section_name.rsplit('|', 1)
+        rsplitted = section_name.rsplit(os.sep, 1)
         package = {'module_name': rsplitted[-1]}
         if len(rsplitted) > 1:
             packages.setdefault(rsplitted[0], []).append(package)
         else:
             packages.setdefault('', []).append(package)
+    return packages
 
-    for package_name, modules in packages.items():
-        parts__init__template = codecs.open(os.path.join('templates', 'parts__init__.mustache'), 'r',
-                                            encoding='utf-8').read()
-        parts__init__template = pystache.parse(parts__init__template)
 
-        parts__init__str = pystache.render(parts__init__template, context={'sections': modules})
-        if package_name == '':
-            parts__init__ = os.path.join(library_path, 'parts', '__init__.py')
-        else:
-            parts__init__ = os.path.join(library_path, 'parts', package_name, '__init__.py')
-        ensure_exists(os.path.dirname(parts__init__))
-        with codecs.open(parts__init__, 'w', encoding='utf-8') as parts__init__file:
-            parts__init__file.write(parts__init__str)
+def _get_sections(parts):
+    sections = {}
+    for key, value in flatten(parts.parts, sep=os.sep).items():
+        rsplitted = key.rsplit(os.sep, 1)
+        description = rsplitted[-1]
+        sections.setdefault(rsplitted[0], {})[description] = value
+    return sections
 
-    for section_name, section_parts in sections.items():
-        parts_list = []
-        bar = Bar('section %s ...' % section_name, max=len(section_parts))
-        for description in section_parts:
-            parts_list.append(get_part_dict(description))
-            bar.next()
-        bar.finish()
 
-        parts_list = [x for x in parts_list if x != {}]
+def generate_section(parts, parts_dir, section_name, section_parts):
+    """ generate all the sections in ldraw.library.parts namespace"""
+    parts_list = []
+    progress_bar = Bar('section %s ...' % section_name, max=len(section_parts))
+    for description in section_parts:
+        parts_list.append(get_part_dict(parts, description))
+        progress_bar.next()
+    progress_bar.finish()
+    parts_list = [x for x in parts_list if x != {}]
+    if section_name == 'others':
+        parts_list.sort(key=lambda r: r.get('category'))
+        for name, grouped in itertools.groupby(parts_list, key=lambda r: r.get('category')):
+            grouped = list(grouped)
+            if name is None:
+                name = 'others'
 
-        def module_name(section):
-            return section.replace('|', os.sep)
+            write_section_file(parts_dir, grouped, name)
+    else:
+        write_section_file(parts_dir, parts_list, section_name)
 
-        def write_section_file(list_of_parts, mod_name):
-            list_of_parts.sort(key=lambda o: o['description'])
-            part_str = pystache.render(parts_template, context={'parts': list_of_parts})
-            parts_py = os.path.join(parts_dir, mod_name + '.py')
-            ensure_exists(os.path.dirname(parts_py))
-            with codecs.open(parts_py, 'w', encoding='utf-8') as generated_file:
-                generated_file.write(part_str)
 
-        if section_name == 'others':
-            parts_list.sort(key=lambda r: r.get('category'))
-            for name, grouped in itertools.groupby(parts_list, key=lambda r: r.get('category')):
-                grouped = list(grouped)
-                if name is None:
-                    name = 'others'
-
-                write_section_file(grouped, module_name(name))
-        else:
-            write_section_file(parts_list, module_name(section_name))
+def generate_parts__init__(library_path, modules, package_name):
+    """ generate the appropriate __init__.py to make submodules in ldraw.library.parts """
+    parts__init__str = pystache.render(PARTS__INIT__TEMPLATE, context={'sections': modules})
+    if package_name == '':
+        parts__init__ = os.path.join(library_path, 'parts', '__init__.py')
+    else:
+        parts__init__ = os.path.join(library_path, 'parts', package_name, '__init__.py')
+    ensure_exists(os.path.dirname(parts__init__))
+    with codecs.open(parts__init__, 'w', encoding='utf-8') as parts__init__file:
+        parts__init__file.write(parts__init__str)
