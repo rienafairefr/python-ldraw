89f6b1646da4d18e2d6b04de33808c77d68925d9
rienafairefr <rienafairefr@gmail.com>
2019-07-16 11:09:52 +0200

    fix some imports & duplicat requirements

--- local_gen.py
+++ local_gen.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import os
 
-from ldraw import download_main, get_data_dir
+from ldraw.dirs import get_data_dir
+from ldraw.download import download_main
 from ldraw.library_gen import library_gen_main
-from appdirs import AppDirs
 # useful for autocompletion in some IDEs
 
 parts_lst = os.path.join(get_data_dir(), 'ldraw', 'parts.lst')
