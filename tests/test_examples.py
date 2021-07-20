32f47c5a251d1345af69ab00c7c72027813c2e84
Matthieu Berthomé <matthieu@mmea.fr>
2021-07-20 23:59:42 +0200

    syntax

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -15,7 +15,7 @@ from ldraw.config import Config
 @pytest.fixture(scope="module")
 def test_ldraw2_config():
     generated_path = tempfile.mkdtemp(prefix=datetime.utcnow().isoformat())
-    print(f'{generated_path=}')
+    print(f'generated_path={generated_path}')
     config = Config(
         ldraw_library_path=os.path.join("tests", "test_ldraw2"),
         generated_path=generated_path
